9d883d4aa9bf0695783f89e5b2f92da2deced6a2
TaranChowdhury <81396452+TaranChowdhury@users.noreply.github.com>
2021-11-03 11:42:36 -0700

    Update DataModel.java

--- DataModel.java
+++ DataModel.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeListener;
+
+public class DataModel {
+	private ArrayList<Integer> stateA;
+	private ArrayList<Integer> stateB;
+	private Enum currentPlayer;
+	private Boolean turnOver;
+	private ArrayList<ChangeListener> boardReference;
+	
+	public DataModel()
+	{
+		
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	
+}
 
