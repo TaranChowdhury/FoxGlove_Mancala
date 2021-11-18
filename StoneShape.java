bf346cc997b0ff66a0551337276d968a94cd6b6d
townsag <86760109+townsag@users.noreply.github.com>
2021-11-17 16:54:40 -0800

    Update StoneShape.java

--- StoneShape.java
+++ StoneShape.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 
 public interface StoneShape {
-    public StoneShape cloneAt(int x, int y);
+    public StoneShape cloneAt(double x, double y);
     public void draw(Graphics2D g2);
+    public void fill(Graphics2D g2);
 }
