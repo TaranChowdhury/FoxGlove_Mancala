520c51a9a22373bbd1ef94bebad5064dc71a94be
townsag <86760109+townsag@users.noreply.github.com>
2021-11-17 16:53:23 -0800

    Update PrimaryFrameTester.java

--- PrimaryFrameTester.java
+++ PrimaryFrameTester.java
@@ -7,9 +7,12 @@ public class PrimaryFrameTester {
     public static void main(String[] args){
 
         DataModel temp = new DataModel();
-        Formatter myFormatter = new Formatter(Color.BLUE,new Rectangle2D.Double(),30);
+        //Formatter myFormatter = new Formatter(Color.BLUE,new Rectangle2D.Double(),30);
+        Formatter stoneShapeFormatter = new Formatter(Color.BLUE, new ConcreteCircle(30,0,0),30);
+        Formatter starShapeFormatter = new Formatter(Color.GREEN, new ConcreteStar(30,0,0),30);
 
-        PrimaryFrame myPF = new PrimaryFrame(temp,myFormatter);
+        PrimaryFrame myPF = new PrimaryFrame(temp,stoneShapeFormatter);
+        //PrimaryFrame myPF = new PrimaryFrame(temp,starShapeFormatter);
 
         myPF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myPF.setVisible(true);
