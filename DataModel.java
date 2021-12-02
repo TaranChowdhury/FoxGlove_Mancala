4ebf049a25ad3ecf9f515e6aa6ea4a13982940e0
townsag <86760109+townsag@users.noreply.github.com>
2021-12-01 16:50:49 -0800

    Update DataModel.java

--- DataModel.java
+++ DataModel.java
@@ -1,56 +1,133 @@
+package Model;
+
 import java.util.ArrayList;
+import View.Formatter;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class DataModel {
-	private ArrayList<Integer> stateA;
-	private ArrayList<Integer> stateB;
-	private Boolean turnOver;
-	private ArrayList<ChangeListener> boardReference;
+    //private ArrayList<Integer> stateA;
+    //private ArrayList<Integer> stateB;
 
-	private enum currentPlayer {
-		player1, player2;
-	}
+    private PlayerInfo player1;
+    private PlayerInfo player2;
 
-	// need to figure out how to configure and add data
-	// stateA and stateB
-	public DataModel() {
-		stateA = new ArrayList<Integer>();
-		stateB = new ArrayList<Integer>();
-		turnOver = false;
-		boardReference = new ArrayList<ChangeListener>();
-	}
+    private Boolean turnOver;
+    private ArrayList<ChangeListener> myListeners;
 
-	/**
-	 * Attach a listener to the Model
-	 * 
-	 * @param cL the listener
-	 */
-	public void attach(ChangeListener cl) {
-		boardReference.add(cl);
-	}
+    private int startingNumStones;
+    private Formatter selectedFormat;
 
-	/**
-	 * Constructs a DataModel object
-	 * 
-	 * @return the data in an ArrayList
-	 */
-	public ArrayList<Integer> getData() {
-		return (ArrayList<Integer>) (stateB.clone());
-	}
+    private boolean isFormatFrameVisible;
+    private boolean isFormatFrameClosed;
 
-	/**
-	 * Add data to the model
-	 * 
-	 * @param s the String to be added
-	 */
-	public void update() {
-		// Code to update data.
-		// still need to work on this.
-		for (ChangeListener listener : boardReference) {
-			listener.stateChanged(new ChangeEvent(this));
-		}
-	}
+    private boolean isPrimaryFrameVisible;
+    private boolean isPrimaryFrameClosed;
+
+    private boolean isStoneNumFrameVisible;
+    private boolean isStoneNumFrameClosed;
+
+    private enum currentPlayer {
+        player1, player2;
+    }
+
+    // need to figure out how to configure and add data
+    // stateA and stateB
+    public DataModel() {
+        //stateA = new ArrayList<Integer>();
+        //stateB = new ArrayList<Integer>();
+
+        player1 = new PlayerInfo(1);
+        player2 = new PlayerInfo(2);
+
+        turnOver = false;
+        myListeners = new ArrayList<ChangeListener>();
+
+
+        isFormatFrameVisible = true;
+        isFormatFrameClosed = false;
+
+        isPrimaryFrameVisible = false;
+        isPrimaryFrameClosed = false;
+
+        isStoneNumFrameVisible = false;
+        isStoneNumFrameClosed = false;
+    }
+
+    public void setSelectedFormat(Formatter sF){
+        selectedFormat = sF;
+    }
+
+    public Formatter getSelectedFormat(){return selectedFormat; }
+
+    public int getStartingNumStones() { return startingNumStones; }
+    public void setStartingNumStones(int startingNum){
+        this.startingNumStones = startingNum;
+        player1.setAllValues(startingNumStones);
+        player2.setAllValues(startingNumStones);
+    }
+
+    public void addStoneTest(int playerNum, int holeIndex){
+        if(playerNum == 1){
+            player1.setHoleValue(holeIndex, player1.getHoleValue(holeIndex) + 1);
+            player1.setMancalaValue(player1.getMancala() + 1);
+        } else if (playerNum == 2){
+            player2.setHoleValue(holeIndex, player2.getHoleValue(holeIndex) + 1);
+            player2.setMancalaValue(player2.getMancala() + 1);
+        }
+        notifyListeners();
+    }
+
+    public InfoWrapper getPlayerInfo(){ return new InfoWrapper(player1,player2); }
+
+    public boolean isFormatFrameVisible() {return isFormatFrameVisible;}
+    public void setFormatFrameVisible(boolean formatFrameVisible) {isFormatFrameVisible = formatFrameVisible;}
+    public boolean isFormatFrameClosed() {return isFormatFrameClosed;}
+    public void setFormatFrameClosed(boolean formatFrameClosed) {
+        isFormatFrameClosed = formatFrameClosed;
+        notifyListeners();
+    }
+
+    public boolean isPrimaryFrameVisible() {return isPrimaryFrameVisible;}
+    public void setPrimaryFrameVisible(boolean primaryFrameVisible) {
+        isPrimaryFrameVisible = primaryFrameVisible;
+        //values of the two player info objects have already been initialized
+        //state changed in primary frame will use accessors in datamodel to get
+        //the values associated with each hole
+        notifyListeners();
+    }
+    public boolean isPrimaryFrameClosed() {return isPrimaryFrameClosed;}
+    public void setPrimaryFrameClosed(boolean primaryFrameClosed) {
+        isPrimaryFrameClosed = primaryFrameClosed;
+        notifyListeners();
+    }
+
+    public boolean isStoneNumFrameVisible() {return isStoneNumFrameVisible;}
+    public void setStoneNumFrameVisible(boolean stoneNumFrameVisible) {
+        isStoneNumFrameVisible = stoneNumFrameVisible;
+        notifyListeners();
+    }
+    public boolean isStoneNumFrameClosed() {return isStoneNumFrameClosed;}
+    public void setStoneNumFrameClosed(boolean stoneNumFrameClosed) {
+        isStoneNumFrameClosed = stoneNumFrameClosed;
+        notifyListeners();
+    }
+
+    /**
+     * Attach a listener to the Model
+     *
+     * @param cl the listener
+     */
+    public void attach(ChangeListener cl) {
+        myListeners.add(cl);
+    }
+
+    private void notifyListeners(){
+        for(ChangeListener cl : myListeners){
+            cl.stateChanged(new ChangeEvent(this));
+        }
+    }
+
 
 }
